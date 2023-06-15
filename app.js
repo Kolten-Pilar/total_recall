cbc8d9239d441ea0983081f391f4086208a44f01
Kolten ✔️ <kolten.pilar@gmail.com>
2023-06-15 11:57:44 -0400

    loops B

--- app.js
+++ app.js
@@ -115,6 +115,14 @@
 // Print out the numbers that are within the range of 1 - 100
 // Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
 
+      for(let i = 1; i <= 100; i++) {
+         if (i % 2 === 0) {
+            console.log(`${i} is even`);
+         } else {
+            console.log(i);
+         }
+      }
+
 
 
 // ### C. Give me Five
