a00f7e7a3470e77b8fdffa4c9c469fe5cb74e933
Kolten ✔️ <kolten.pilar@gmail.com>
2023-06-15 11:45:03 -0400

    driver'd ed

--- app.js
+++ app.js
@@ -69,6 +69,14 @@
 // Make a variable that holds a person's age; be semantic
 // Write code that will print out "Here are the keys!", if the age is 16 years or older, or, if the age is younger than 16, a message should print "Sorry, you're too young."
 
+   let myAge = 28;
+
+   if (myAge >= 16) {
+      console.log(('Here are the Keys!'));
+   } else {
+      console.log((`Sorry, You're too young.`));
+   }
+
 
 // ## Loops
 
